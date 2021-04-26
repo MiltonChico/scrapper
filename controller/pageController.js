5867b17cacfb7f64c425d2e84251df08e8a302ab
MiltonChico <milton.chico@outlook.com>
2021-04-26 13:45:44 -0300

    Testing Zillow

--- controller/pageController.js
+++ controller/pageController.js
@@ -1,10 +1,9 @@
-const pageScraper = require('./pageScraper');
+const pageScraper = require('../scrappers/pageScraper');
 async function scrapeAll(browserInstance){
     let browser;
     try{
         browser = await browserInstance;
         await pageScraper.scraper(browser);
-
     }
     catch(err){
         console.log("Could not resolve the browser instance => ", err);
