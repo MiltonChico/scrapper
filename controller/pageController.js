f9c23003979a5ba797822d91770e0bdaeb824205
MiltonChico <milton.chico@outlook.com>
2021-04-26 14:32:15 -0300

    Scrap Ok

--- controller/pageController.js
+++ controller/pageController.js
@@ -1,15 +1,17 @@
 const bookScraper = require('../scrappers/bookScrapper');
 const googleScraper = require('../scrappers/googleScrapper');
 const zillowScrapper = require('../scrappers/zillowScrapper');
+const pageScraper = require('../scrappers/pageScraper');
 
 async function scrapeAll(browserInstance){
     let browser;
     
     try{
         browser = await browserInstance;
-        await bookScraper.scraper(browser);
-        await zillowScrapper.scraper(browser);
-        await googleScraper.scraper(browser);
+        
+        await pageScraper.scraper(browser);
+        // await zillowScrapper.scraper(browser);
+        // await googleScraper.scraper(browser);
     }
     catch(err){
         console.log("Could not resolve the browser instance => ", err);
