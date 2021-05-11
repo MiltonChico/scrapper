5120c45d51e87d3878ec603fd1e1c71164e69484
MiltonChico <milton.chico@outlook.com>
2021-05-11 10:31:16 -0300

    must be fine

--- index.js
+++ index.js
@@ -16,22 +16,22 @@ class Scrapper {
         var i;
         for( i = 0; i < AllRealtors.length ; i++) {
             
-            // if(AllRealtors[i].zillow){
-            //     const zillowReviews = await ZillowController.start(AllRealtors[i].zillow, browserInstance);
-            //     //await AirtableController.save(zillowReviews);
-            // }
-            // if(AllRealtors[i].realtor){
-            //     const realtorReviews = await RealtorController.start(AllRealtors[i].realtor, browserInstance);
-            //     //await AirtableController.save(zillowReviews);
-            // }
+            if(AllRealtors[i].zillow){
+                const zillowReviews = await ZillowController.start(AllRealtors[i].zillow, browserInstance);
+                //await AirtableController.save(zillowReviews);
+            }
+            if(AllRealtors[i].realtor){
+                const realtorReviews = await RealtorController.start(AllRealtors[i].realtor, browserInstance);
+                //await AirtableController.save(zillowReviews);
+            }
+            if(AllRealtors[i].google){
+                const googleReviews = await GoogleController.start(AllRealtors[i].google, browserInstance);
+                //await AirtableController.save(zillowReviews);
+            }       
             if(AllRealtors[i].yelp){
                 const yelpReviews = await YelpController.start(AllRealtors[i].yelp, browserInstance);
                 //await AirtableController.save(zillowReviews);
             }
-            // if(AllRealtors[i].google){
-            //     const googleReviews = await GoogleController.start(AllRealtors[i].google, browserInstance);
-            //     //await AirtableController.save(zillowReviews);
-            // }       
         }
     } 
 }
